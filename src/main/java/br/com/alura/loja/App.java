11cb5e2159236dde992ab4dfda6b8971a3fb8ee1
joaohab <joao_hab@hotmail.com>
2023-08-23 21:11:10 -0300

    23-08-2023

---
+++ src/main/java/br/com/alura/loja/App.java
@@ -0,0 +1,13 @@
+package br.com.alura.loja;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
